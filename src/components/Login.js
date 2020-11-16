4602752baf52041ed9cb6679a9f882695b05b903
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-11-16 17:13:51 +0530

    Uncontrolled form handling done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+  }
+
+  handleFormsSubmit = (e) => {
+    e.preventDefault();
+    console.log('this.emailInputRef', this.emailInputRef);
+    console.log('this.passwordInputRef', this.passwordInputRef);
+  };
+
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            ref={this.emailInputRef}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            ref={this.passwordInputRef}
+          />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button onClick={this.handleFormsSubmit}>Log In</button>
         </div>
       </form>
     );
