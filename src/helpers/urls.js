d776dcc67256dc7bc5d1ab5a1e8af1c2d4c16baf
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2021-02-03 16:10:43 +0530

    Add user as Friend

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -9,4 +9,8 @@ export const APIUrls = {
 
   userProfile: (userId) => `${API_ROOT}/users/${userId}`,
   userFriends: (userId) => `${API_ROOT}/friendship/fetch_user_friends`,
+  addFriend: (userId) =>
+    `${API_ROOT}friendship/create_friendship?user_id=${userId}`,
+  removeFriend: (userId) =>
+    `${API_ROOT}/friendship/remove_friendship?user_id=${userId}`,
 };
