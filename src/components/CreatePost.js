70427d236e98c7ac8d9e3dc73e14232466e15227
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2021-02-08 22:10:41 +0530

    Add Post component

---
+++ src/components/CreatePost.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+  }
+
+  handleOnClick = () => {
+      //dispatch an action to create post
+  }
+
+  handleChange = (e) => {
+      this.setState({
+          content = e.target.value
+      })
+  };
+
+  render() {
+    return (
+      <div className="create-post">
+        <textarea
+          className="add-post"
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
+
+        <button id="add-post-btn" onClick={this.handleOnClick}>Add Post</button>
+      </div>
+    );
+  }
+}
+
+export default CreatePost;
